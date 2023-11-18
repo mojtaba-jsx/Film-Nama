ff4f5e2160346718790d620ae4486cf0de4246ef
mojtaba.jsx <mojtaba.jsx@gmail.com>
2023-11-18 09:49:27 +0330

    [JS]- add modal logic

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 //! Search Modal Logic
 const modalSearch = document.querySelector('.search-modal');
 const searchIcon =document.querySelector('.navabr__search-icon-svg');
+const closeModalBtn = document.querySelector('.search-modal__close');
+const modalContainer = document.querySelector('.modal-container');
+
 
 searchIcon.addEventListener('click',()=>{
     modalSearch.classList.add('modal-show');
+    // modalContainer.style.filter = 'blur(10px)'
+})
+
+closeModalBtn.addEventListener('click',()=>{
+    modalSearch.classList.remove('modal-show');
 })
