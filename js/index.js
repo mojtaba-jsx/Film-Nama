d8f7375f688aab6732bf9e2bf253ef7141c3b3ab
mojtaba.jsx <mojtaba.jsx@gmail.com>
2023-11-18 10:22:47 +0330

    [HTML-CSS-JS]- add notif box

--- js/index.js
+++ js/index.js
@@ -4,12 +4,26 @@ const searchIcon =document.querySelector('.navabr__search-icon-svg');
 const closeModalBtn = document.querySelector('.search-modal__close');
 const modalContainer = document.querySelector('.modal-container');
 
+const notifModalCloseBtn = document.querySelector('.notif-modal__close');
+const notifBox = document.querySelector('.notif-box');
+const navbarBellIcon = document.querySelector('.navbar__bell-icon-svg');
+let flag = false;
 
 searchIcon.addEventListener('click',()=>{
     modalSearch.classList.add('modal-show');
-    // modalContainer.style.filter = 'blur(10px)'
+    notifBox.style.display='none';
 })
 
 closeModalBtn.addEventListener('click',()=>{
     modalSearch.classList.remove('modal-show');
+})
+
+navbarBellIcon.addEventListener('click',()=>{
+    if(!flag){
+        notifBox.style.display='flex';
+        flag=true;
+    }else{
+        notifBox.style.display='none';
+        flag=false;
+    }
 })
