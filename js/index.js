b99d91c25207e3250c951b04d57967de4f5b6168
mojtaba.jsx <mojtaba.jsx@gmail.com>
2023-11-21 13:39:08 +0330

    change swiper js config and next and prev button

--- js/index.js
+++ js/index.js
@@ -145,8 +145,11 @@ mobileMenuLangIcon.addEventListener('click',()=>{
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
+    autoplay: {
+        delay: 2000,
+      },
   
     // If we need pagination
     pagination: {
