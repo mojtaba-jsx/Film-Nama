e92ff3682bdec19ebc12d98d0adf81707b1fe5aa
mojtaba.jsx <mojtaba.jsx@gmail.com>
2023-11-23 10:18:21 +0330

    [Responsive]- responsive recommend section

--- js/index.js
+++ js/index.js
@@ -190,6 +190,71 @@ const swiper1 = new Swiper('.swiper-container-1', {
     pagination: {
       el: '.swiper-pagination',
     },
+    centeredSlides: true,
+    // centeredSlidesBounds: true,
+
+      breakpoints: {
+        300: {
+          slidesPerView: 1,
+          spaceBetween: 1,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 1,
+        },
+        350: {
+          slidesPerView: 1,
+          spaceBetween: 1,
+        },
+        360: {
+          slidesPerView: 1,
+          spaceBetween: 1,
+        },
+        390: {
+          slidesPerView: 1,
+          spaceBetween: 1,
+        },
+        460: {
+          slidesPerView: 2,
+          spaceBetween: 180,
+        },
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 290,
+        },
+        580: {
+          slidesPerView: 2,
+          spaceBetween: 100,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 220,
+        },
+        810: {
+          slidesPerView: 3,
+          spaceBetween: 320,
+        },
+        906: {
+          slidesPerView: 3,
+          spaceBetween: 320,
+        },
+        940: {
+          slidesPerView: 4,
+          spaceBetween: 320,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 300,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 200,
+        },
+      },
 
   
     // Navigation arrows
