919c56aa37106dcc84c18bea26b20fd2a1d4263d
mojtaba.jsx <mojtaba.jsx@gmail.com>
2023-12-16 09:40:56 +0330

    change slider view

--- js/movie.js
+++ js/movie.js
@@ -10,6 +10,18 @@ const swiper = new Swiper('.swiper-movie', {
     //   autoplay: {
     //   delay: 2000,
     // },
+    
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },   
 
   // If we need pagination
   pagination: {
